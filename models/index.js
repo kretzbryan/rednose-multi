732a9eb4b67181c1ad5f548061acd7e14a1a9a20
bryankretz <kretzbryan@gmail.com>
2020-07-09 16:44:28 -0700

    model index set up

--- models/index.js
+++ models/index.js
@@ -1,1 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost:27017/circusnetwork';
+
+mongoose.connect(process.env.MONGODB_URI || connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+.then(() => {
+    console.log('MongoDB Connected!');
+})
+.catch(err => {
+    console.log(err);
+})
+
+module.exports = {
+    Gig: require('./Gig'),
+    Post: require('./Post'),
+    Profile: require('./Profile'),
+    // User: require('./User')
+}
